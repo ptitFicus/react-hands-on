732b0ee4d9f79743e1aa32caf8561d5d5635ff27
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-12 22:52:43 +0100

    feat: context based favorites

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Search } from "./pages/Search";
 import { Artist } from "./pages/Artist";
+import { useEffect, useState } from "react";
+import { FavoriteContext } from "./FavoriteContext";
 
 const router = createBrowserRouter([
   {
@@ -12,7 +14,38 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [favorites, setFavorite] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorite = ({ artist, album }) => {
+    setFavorite((favs) => [...favs, { artist, album }]);
+  };
+
+  const removeFavorite = ({ artist, album }) => {
+    setFavorite((favs) =>
+      favs.filter(
+        ({ artist: fartist, album: falbum }) =>
+          artist !== fartist || album !== falbum
+      )
+    );
+  };
+
+  return (
+    <FavoriteContext.Provider
+      value={{
+        favorites: favorites,
+        addFavorite,
+        removeFavorite,
+      }}
+    >
+      <RouterProvider router={router} />
+    </FavoriteContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,13 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { string, oneOf } from "prop-types";
+import { FavoriteContext } from "../FavoriteContext";
 
 export function Cover({ artist, album, size }) {
   const [url, setUrl] = useState(undefined);
   useEffect(() => {
     albumArt(artist, { album: album, size: size }).then((url) => setUrl(url));
   }, [artist, album, size]);
+  const { favorites, addFavorite, removeFavorite } =
+    useContext(FavoriteContext);
+  console.log("cover favorties", favorites);
+  const isFavorite = Boolean(
+    favorites.find(
+      ({ artist: fartist, album: falbum }) =>
+        fartist === artist && falbum === album
+    )
+  );
 
-  return url ? <img src={url} title={album} /> : <span className="loader" />;
+  return url ? (
+    <div
+      className={`star ${isFavorite ? "active" : ""}`}
+      onClick={() => {
+        if (isFavorite) {
+          removeFavorite({ artist, album });
+        } else {
+          addFavorite({ artist, album });
+        }
+      }}
+    >
+      <img src={url} title={album} />
+    </div>
+  ) : (
+    <span className="loader" />
+  );
 }
 Cover.propTypes = {
   artist: string.isRequired,
