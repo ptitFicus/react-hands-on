c3a9cee1451960fd784d2dda5d557ec646bf6dea
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-17 10:40:22 +0100

    feat: style + remove useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
-
-import logo from './assets/logo.jpg'
+import logo from "./assets/logo.jpg";
 import { ArtistTable } from "./components/ArtistTable";
+import "./App.css";
 
 export const App = () => {
-    return <>
-        <header>
-            <nav>
-                <a href={'/'}>
-                    <img src={logo} alt="logo" height="70" style={{ borderRadius: '50%'}} />
-                </a>
-                <ul>
-                    <li>
-                        <a href="/search">search</a>
-                    </li>
-                    <li>
-                        <a href="/favorites">see favorites</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <ArtistTable />
-        </main>
+  return (
+    <>
+      <header>
+        <nav>
+          <a href={"/"}>
+            <img
+              src={logo}
+              alt="logo"
+              height="70"
+              style={{ borderRadius: "50%" }}
+            />
+          </a>
+          <ul>
+            <li>
+              <a href="/search">search</a>
+            </li>
+            <li>
+              <a href="/favorites">see favorites</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <ArtistTable />
+      </main>
     </>
-}
+  );
+};
--- src/components/ArtistTable.jsx
+++ src/components/ArtistTable.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-
-import { artistsWithAlbums } from '../utils/utils.js';
-
+import { artistsWithAlbums } from "../utils/utils.js";
 
 export const ArtistTable = () => {
-    return <table>
-        <thead>
-            <tr>
-                <td>Artist ({artistsWithAlbums.length})</td>
-                <td>Albums</td>
-            </tr>
-        </thead>
-        <tbody>
-            {artistsWithAlbums?.map(artist => 
-                <tr key={artist.name}>
-                    <td>{artist.name}</td>
-                    <td>{artist.albums.length ? artist.albums.length : 'No Albums'}</td>
-                </tr>)}
-        </tbody>
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Artist ({artistsWithAlbums.length})</td>
+          <td>Albums</td>
+        </tr>
+      </thead>
+      <tbody>
+        {artistsWithAlbums?.map((artist) => (
+          <tr key={artist.name}>
+            <td>{artist.name}</td>
+            <td>{artist.albums.length ? artist.albums.length : "No Albums"}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
+  );
 };
