f4adf50154dcdb644acf4d3d17530c42a2f9867e
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-17 10:28:57 +0100

    feat: uniformize data file with others steps + add style

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
-import logo from './assets/logo.jpg'
-import artist from "./utils/data/Artist.json";
-
-
+import logo from "./assets/logo.jpg";
+import { artistsWithAlbums } from "./utils/utils.js";
+import "./App.css";
 
 const makeHeader = () => {
-  const header = document.createElement('header')
+  const header = document.createElement("header");
 
-  const nav = document.createElement('nav');
+  const nav = document.createElement("nav");
 
-  const basePath = document.createElement('a');
-  basePath.href = '/'
+  const basePath = document.createElement("a");
+  basePath.href = "/";
   basePath.innerHTML = `<img src=${logo} height="70" style="border-radius:50%;" />`;
   nav.appendChild(basePath);
 
-  const menuList = document.createElement('ul');
-  const search = document.createElement('li');
+  const menuList = document.createElement("ul");
+  const search = document.createElement("li");
   search.innerHTML = `<a href="/search">search</a>`;
 
-  const fav = document.createElement('li');
+  const fav = document.createElement("li");
   fav.innerHTML = `<a href="/favorites">see favorites</a>`;
 
   menuList.appendChild(search);
@@ -28,17 +27,17 @@ const makeHeader = () => {
   header.appendChild(nav);
 
   return header;
-}
+};
 
-const rootDiv = document.getElementById('root');
+const rootDiv = document.getElementById("root");
 
 const makeTable = (data, columns = []) => {
-  const table = document.createElement('table');
-  const head = document.createElement('thead');
-  const trHead = document.createElement('tr');
+  const table = document.createElement("table");
+  const head = document.createElement("thead");
+  const trHead = document.createElement("tr");
 
-  columns.forEach(col => {
-    const tdHead = document.createElement('td')
+  columns.forEach((col) => {
+    const tdHead = document.createElement("td");
     tdHead.innerText = col;
     trHead.appendChild(tdHead);
   });
@@ -46,23 +45,22 @@ const makeTable = (data, columns = []) => {
   head.appendChild(trHead);
   table.appendChild(head);
 
-  data.forEach(artist => {
-    const tr = document.createElement('tr');
-    columns.forEach(col => {
-      const td = document.createElement('td')
+  data.forEach((artist) => {
+    const tr = document.createElement("tr");
+    columns.forEach((col) => {
+      const td = document.createElement("td");
       td.innerText = artist[col];
       tr.appendChild(td);
-    })
-    table.appendChild(tr)
+    });
+    table.appendChild(tr);
   });
 
   return table;
+};
 
-}
+const main = document.createElement("main");
 
-const main = document.createElement('main');
-
-main.appendChild(makeTable(artist, ['Name']))
+main.appendChild(makeTable(artistsWithAlbums, ["name"]));
 
 rootDiv.appendChild(makeHeader());
-rootDiv.appendChild(main)
+rootDiv.appendChild(main);
---
+++ src/utils/utils.js
@@ -0,0 +1,42 @@
+import artists from "./data/Artist.json";
+import albums from "./data/Album.json";
+
+export function search(text) {
+  const upper = text?.toUpperCase();
+  return new Promise((resolve) => {
+    if (!text || text?.length === 0) {
+      resolve([]);
+    } else {
+      const res = artistsWithAlbums.filter(({ name }) =>
+        name.toUpperCase().includes(upper)
+      );
+      setTimeout(() => {
+        resolve(res);
+      }, 1000);
+    }
+  });
+}
+
+export function add(artist, albums) {
+  return new Promise((resolve, reject) => {
+    if (
+      artistsWithAlbums.find(
+        ({ name }) => name.toUpperCase() === artist?.toUpperCase()
+      )
+    ) {
+      reject(new Error("Artist already exist"));
+    } else {
+      artistsWithAlbums.push({ name: artist, albums: albums });
+      localStorage.setItem("artists", JSON.stringify(artistsWithAlbums));
+      resolve({ artist, albums });
+    }
+  });
+}
+
+export const artistsWithAlbums =
+  localStorage.getItem("artists") !== null
+    ? JSON.parse(localStorage.getItem("artists"))
+    : artists.map(({ ArtistId, Name }) => {
+        const artistAlbums = albums.filter((al) => al.ArtistId === ArtistId);
+        return { name: Name, albums: artistAlbums.map(({ Title }) => Title) };
+      });
