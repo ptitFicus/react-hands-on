904e7871590810a2537afa92c06183b1d62005e7
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-17 11:25:20 +0100

    feat: misc linting fix

--- src/components/ArtistTable.jsx
+++ src/components/ArtistTable.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import { shape, arrayOf, string, array, func } from "prop-types";
 
-
-
-export const ArtistTable = ({artists, onArtistSelection}) => {
-    return <table>
-        <thead>
-            <tr>
-                <td>Artist ({artists.length})</td>
-                <td>Albums</td>
-                <td>Actions</td>
-            </tr>
-        </thead>
-        <tbody>
-            {artists?.map(artist => 
-                <tr key={artist.name}>
-                    <td>{artist.name}</td>
-                    <td>{artist.albums.length ? artist.albums.length : 'No Albums'}</td>
-                    <td><button onClick={() => onArtistSelection(artist)}>Détails</button></td>
-                </tr>)}
-        </tbody>
+export const ArtistTable = ({ artists, onArtistSelection }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Artist ({artists.length})</td>
+          <td>Albums</td>
+          <td>Actions</td>
+        </tr>
+      </thead>
+      <tbody>
+        {artists?.map((artist) => (
+          <tr key={artist.name}>
+            <td>{artist.name}</td>
+            <td>{artist.albums.length ? artist.albums.length : "No Albums"}</td>
+            <td>
+              <button onClick={() => onArtistSelection(artist)}>Détails</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
+  );
+};
+
+ArtistTable.propTypes = {
+  artists: arrayOf(
+    shape({
+      name: string,
+      albums: array,
+    })
+  ),
+  onArtistSelection: func,
 };
