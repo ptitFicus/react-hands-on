95d590d792d6a816867dbbc4a208177ff6daf9fe
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-17 10:45:59 +0100

    feat: improve functionnalities
    * Better filtering (uppercase comparison & includes instead of startsWith)
    * Allow closing details
    * Add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,62 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-import logo from './assets/logo.jpg'
+import logo from "./assets/logo.jpg";
+import "./App.css";
 import { ArtistTable } from "./components/ArtistTable";
 import { ArtistDetail } from "./components/ArtistDetail";
-import { artistsWithAlbums } from './utils/utils.js';
+import { artistsWithAlbums } from "./utils/utils.js";
 
 export const App = () => {
+  const [artist, setArtist] = useState();
 
-    const [artist, setArtist] = useState();
+  const [filter, setFilter] = useState();
 
-    const [filter, setFilter] = useState();
-
-    return <>
-        <header>
-            <nav>
-                <a href={'/'}>
-                    <img src={logo} alt="logo" height="70" style={{ borderRadius: '50%'}} />
-                </a>
-                <ul>
-                    <li>
-                        <a href="/search">search</a>
-                    </li>
-                    <li>
-                        <a href="/favorites">see favorites</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            {artist && <ArtistDetail artist={artist} />}
-            <input placeholder="Filtrez" onChange={e => {
-                setFilter(e.target.value);
-            }} />
-            <ArtistTable artists={filter && artistsWithAlbums.filter(a => a.name.startsWith(filter)) || artistsWithAlbums} onArtistSelection={value => setArtist(value)}/>
-            
-        </main>
+  return (
+    <>
+      <header>
+        <nav>
+          <a href={"/"}>
+            <img
+              src={logo}
+              alt="logo"
+              height="70"
+              style={{ borderRadius: "50%" }}
+            />
+          </a>
+          <ul>
+            <li>
+              <a href="/search">search</a>
+            </li>
+            <li>
+              <a href="/favorites">see favorites</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        {artist && (
+          <>
+            <ArtistDetail artist={artist} />
+            <button onClick={() => setArtist(undefined)}>Close</button>
+          </>
+        )}
+        <input
+          placeholder="Filtrez"
+          onChange={(e) => {
+            setFilter(e.target.value.toUpperCase());
+          }}
+        />
+        <ArtistTable
+          artists={
+            (filter &&
+              artistsWithAlbums.filter((a) =>
+                a.name.toUpperCase().includes(filter)
+              )) ||
+            artistsWithAlbums
+          }
+          onArtistSelection={(value) => setArtist(value)}
+        />
+      </main>
     </>
-}
+  );
+};
