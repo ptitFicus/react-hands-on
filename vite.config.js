b4530e5c1e47d531c4d2b513e50f8cea6478ad51
Benjamin Cavy <benjamin.cavy@gmail.com>
2023-11-20 00:19:23 +0100

    WIP

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-hands-on/",
+  base: "/react-hands-on/_media",
 });
