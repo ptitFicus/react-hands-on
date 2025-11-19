0495d38d6776f62c84e2e939ac81d305c9f97091
Benjamin Cavy <benjamin.cavy@maif.fr>
2025-11-19 23:41:30 +0100

    correction etape 3

--- src/components/AlbumDisplay.jsx
+++ src/components/AlbumDisplay.jsx
@@ -1,17 +1,63 @@
+import { useEffect, useState } from "react";
+import { fetchCoverImage } from "../utils/utils";
+
 export function AlbumDisplay({ artist, albums }) {
   return (
     <div>
       <h1>{artist}</h1>
-      <h2>Albums</h2>
-      {albums.length === 0 ? (
-        <>No album found for {artist}</>
-      ) : (
-        <ul>
-          {albums.map((a) => (
-            <li key={a}>{a}</li>
-          ))}
-        </ul>
-      )}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        {albums.length === 0 ? (
+          <>No album found for {artist}</>
+        ) : (
+          <>
+            {albums.map((a) => (
+              <div key={a}>
+                <Cover artist={artist} name={a} />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
+}
+
+function Cover({ artist, name }) {
+  const [loadStatus, setLoadStatus] = useState({ status: "loading" });
+
+  useEffect(() => {
+    fetchCoverImage(artist, name, "medium")
+      .then((url) => {
+        console.log("url", url);
+        setLoadStatus({ status: "loaded", url: url });
+      })
+      .catch((err) => {
+        setLoadStatus({ status: "failed" });
+      });
+  }, [artist, name]);
+
+  switch (loadStatus.status) {
+    case "loading": {
+      return <div className="loader" />;
+    }
+    case "loaded": {
+      return (
+        <div>
+          <h2>{name}</h2>
+          <img src={loadStatus.url} />
+        </div>
+      );
+    }
+    case "failed": {
+      return <div>Failed to load cover</div>;
+    }
+  }
 }
