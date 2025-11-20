3070ed758b6dacc7b05e9ba740cc900ba2594d9f
Benjamin Cavy <benjamin.cavy@maif.fr>
2025-11-20 02:09:26 +0100

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { Artist } from "./pages/Artist";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import { Artists } from "./pages/Artists";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FavoriteContext } from "./FavoriteContext";
+import { Favorites } from "./pages/Favorites";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,11 @@ const router = createBrowserRouter([
         errorElement: <div>This artist does not exist</div>,
       },
       {
+        path: "/favorites",
+        element: <Favorites />,
+        errorElement: <div>Failed to load favorites</div>,
+      },
+      {
         path: "/",
         element: <Artists />,
       },
@@ -25,7 +33,38 @@ const router = createBrowserRouter([
 ]);
 
 export function App() {
-  return <RouterProvider router={router} />;
+  const [favorites, setFavorite] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorite = ({ artist, album }) => {
+    setFavorite((favs) => [...favs, { artist, album }]);
+  };
+
+  const removeFavorite = ({ artist, album }) => {
+    setFavorite((favs) =>
+      favs.filter(
+        ({ artist: fartist, album: falbum }) =>
+          artist !== fartist || album !== falbum
+      )
+    );
+  };
+
+  return (
+    <FavoriteContext.Provider
+      value={{
+        favorites: favorites,
+        addFavorite,
+        removeFavorite,
+      }}
+    >
+      <RouterProvider router={router} />
+    </FavoriteContext.Provider>
+  );
 }
 
 export function Layout() {
--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { fetchCoverImage } from "../utils/utils";
 import { string, oneOf } from "prop-types";
+import { FavoriteContext } from "../FavoriteContext";
 
 export function Cover({ artist, album, size }) {
   const [loadStatus, setLoadStatus] = useState({ status: "loading" });
@@ -15,12 +16,34 @@ export function Cover({ artist, album, size }) {
       });
   }, [artist, album]);
 
+  const { favorites, addFavorite, removeFavorite } =
+    useContext(FavoriteContext);
+  const isFavorite = Boolean(
+    favorites.find(
+      ({ artist: fartist, album: falbum }) =>
+        fartist === artist && falbum === album
+    )
+  );
+
   switch (loadStatus.status) {
     case "loading": {
       return <div className="loader" />;
     }
     case "loaded": {
-      return <img src={loadStatus.url} />;
+      return (
+        <div
+          className={`star ${isFavorite ? "active" : ""}`}
+          onClick={() => {
+            if (isFavorite) {
+              removeFavorite({ artist, album });
+            } else {
+              addFavorite({ artist, album });
+            }
+          }}
+        >
+          <img src={loadStatus.url} />
+        </div>
+      );
     }
     case "failed": {
       if (size === "small") {
